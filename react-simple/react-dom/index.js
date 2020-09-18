be160d1604e2462fc46730350877b456640a5d49
Larry <larrylizhao@gmail.com>
2020-09-18 16:29:41 +0800

    refactoring: Add comment for ReactDom render

--- react-simple/react-dom/index.js
+++ react-simple/react-dom/index.js
@@ -6,15 +6,26 @@ class ReactDOM {
 
     //虚拟dom转为真实dom,并设置dom属性
     _render(element) {
-        if(!element){
+        if(!element || typeof element === 'boolean'){
             return null;
         }
 
+        // 1. 如果element是字符串
         if(typeof element === 'string') {
+            // 创建文本节点
             return document.createTextNode(element);
         }
 
+        // 2. 处理函数组件
+        if (typeof element.type === 'function') {
+            // 1. 创建组件
+
+        }
+
+        // 3. 否则就是虚拟DOM对象
         const {type, config} = element;
+
+        // 由虚拟DOM创建真实DOM，并设置属性
         const dom = document.createElement(type);
         if(config) {
             Object.keys(config).forEach(key => {
@@ -23,26 +34,30 @@ class ReactDOM {
         }
 
         //递归渲染子节点
-        element.children.forEach(child => {
-            this.render(child, dom);
-        })
+        if(element.children) {
+            element.children.forEach(child => {
+                this.render(child, dom);
+            })
+        }
 
+        // 返回创建好的真实DOM
         return dom
     }
 
-    //设置属性
+    // 设置dom节点属性
     setAttribute(dom, key, value) {
         if(key === 'className') {
             key = 'class';
         }
 
+        // 绑定event
         if(/on\w+/.test(key)) {
             key = key.toLowerCase();
             dom[key] = value || '';
-        } else if(key === 'style') {
+        } else if(key === 'style') { // 设置style
             if(!value || typeof value === 'string') {
                 dom.style.cssText = value || '';
-            } else if(value && typeof value === 'object') {
+            } else if(value && typeof value === 'object') { //其他style属性
                 for(let key in value) {
                     if(typeof value[k] === 'number') {
                         dom.style[k] = value[k] + 'px';
@@ -62,7 +77,6 @@ class ReactDOM {
                 dom.removeAttribute(key);
             }
         }
-
     }
 }
 
