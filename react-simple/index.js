b7e7cfb1165f9aae848b9f7789a7dc1110ab86b2
Larry <larrylizhao@gmail.com>
2020-09-18 16:38:51 +0800

    React: ReactDOM可接受的渲染类型

--- react-simple/index.js
+++ react-simple/index.js
@@ -1,12 +1,14 @@
 import React from './react';
 import ReactDOM from './react-dom';
 
+// 1. JSX
 const ele = (
     <div className='active' title='123'>
         Hello <span>React</span>
     </div>
 );
 
+// 2. 函数组件
 const elef = () => {
     return (
         <div className='active' title='123'>
@@ -15,6 +17,7 @@ const elef = () => {
     )
 }
 
+
 function home() {
     return (
         <div className='active' title='123'>
@@ -22,10 +25,30 @@ function home() {
         </div>
     )
 }
+
+// 3. 类组件
+class Home {
+    render() {
+        return (
+            <div className='active' title='123'>
+                Hello <span>React</span>
+            </div>
+        );
+    }
+}
+
 const title = 'title'
 console.log(<home name={title}/>);
 
-ReactDOM.render(<homt name={title}/>, document.querySelector('#root'));
+
+/*
+    ReactDOM.render可接收：
+    1. String, Number
+    2. JSX: 被babel转译为JS Object
+    3. 函数组件
+    4. 类组件
+ */
+ReactDOM.render(<home name={title}/>, document.querySelector('#root'));
 //ReactDOM.render('react', document.querySelector('#root'));
 
 console.log(ele);
